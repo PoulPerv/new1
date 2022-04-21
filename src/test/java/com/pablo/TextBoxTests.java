f90a83d305d6487b9028e2ec619a70b72247156a
PoulPerv <dizel_tdi@bk.ru>
2022-04-21 17:44:32 +0300

    added 7 new strings

--- src/test/java/com/pablo/TextBoxTests.java
+++ src/test/java/com/pablo/TextBoxTests.java
@@ -1,4 +1,35 @@
 package com.pablo;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+//import static com.codeborne.selenide.Selectors.byXpath; //если понадобится скролл
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 public class TextBoxTests {
+    @BeforeAll
+    static void setUp() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
+    @Test
+    void fillFormTest(){
+        open("/text-box");
+
+        String name = "Pablo Black";
+        $("[id=userName]").setValue(name);
+        $("[id=userEmail]").setValue("Pablo@gmail.com");
+        $("[id=currentAddress]").setValue("Street");
+        $("[id=permanentAddress]").setValue("House");
+        $("[id=submit]").click();
+        $("[id=output]").shouldHave(text(name), text("Pablo@gmail.com"), text("Street"), text("House"));
+    }
 }
+
